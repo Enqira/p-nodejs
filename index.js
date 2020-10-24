7350292a888e7dac75778b12f7e3cc0f1b19f0a6
Enqira <moenkira@yahoo.com>
2020-10-25 01:14:29 +0200

    code for area and perimeter

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var rect = {
     perimeter: (x, y) => (2*(x+y)),
     area: (x, y) => (x*y)
-};
-function solveRect(l,b) {
-    console.log("Solving for rectangle width l = " + l "and b = " + b);
-
-    if ( l <= 0 )
 }
+function solveRect(l,b) {
+    console.log("Solving for rectangle width l = " + l + "and b = " + b);
+
+    if ( l <= 0 || b <= 0){
+        console.log("rectangle dimentions should be greater than zero l = " + l + " b = " + b);
+    }
+    else {
+        console.log("The area of the rectangle is " + rect.area(l,b));
+        console.log("The primeter of the rectangle is " + rect.perimeter(l,b));
+    }
+}
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-3, 5);
