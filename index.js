ad009e776def381d5f05e74c3e34bdda13495d2a
Enqira <moenkira@yahoo.com>
2020-10-25 00:50:53 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var rect = {
+    perimeter: (x, y) => (2*(x+y)),
+    area: (x, y) => (x*y)
+};
+function solveRect(l,b) {
+    console.log("Solving for rectangle width l = " + l "and b = " + b);
+
+    if ( l <= 0 )
+}
